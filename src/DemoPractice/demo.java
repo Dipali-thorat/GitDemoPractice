e786baac95df481f235a5b1d2bdaa3b44addf6d0
Com <Com@Com-PC>
2022-11-21 12:56:18 +0530

    add changes from working area to staging area and commet to local repo

---
+++ src/DemoPractice/demo.java
@@ -0,0 +1,17 @@
+package DemoPractice;
+
+public class demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String hungry="yes";
+		if(hungry=="yes")
+		{ 
+			 System.out.println("Eat somethng");
+		}
+		else
+			System.out.println("do work");
+
+	}
+
+}
